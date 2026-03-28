ac58e8a23f29d4d73e911b5495f53aa7e6e7f704
dcheshirecat <danieldinadin@gmail.com>
2026-03-28 22:19:06 +0300

    fix: add Gradle properties to work around SDK 35 D8 issue

--- Assets/_Project/Editor/BuildScript.cs
+++ Assets/_Project/Editor/BuildScript.cs
@@ -80,8 +80,9 @@ namespace EndlessBeloved.Editor
             PlayerSettings.applicationIdentifier = bundleId;
             PlayerSettings.productName = productName;
 
-            // Target SDK 34 (Android 14) - SDK 35 has D8 dexing issues with Unity 2022.3
-            PlayerSettings.Android.targetSdkVersion = AndroidSdkVersions.AndroidApiLevel34;
+            // Target SDK 35 (Android 15) for Google Play Protect compatibility
+            // Cast to int because Unity 2022.3 enum may not have AndroidApiLevel35
+            PlayerSettings.Android.targetSdkVersion = (AndroidSdkVersions)35;
 
             // Set scripting define symbols for this variant
             var currentDefines = PlayerSettings.GetScriptingDefineSymbolsForGroup(
